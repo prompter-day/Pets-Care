b14f5437bb63247671ff083a3b151600d87714a2
손보석 <dya_only@dyaonlys-MacBook-Pro.local>
2023-09-03 17:36:19 +0900

    FEAT: Chat GPT API Chat

--- src/pages/chat/page.tsx
+++ src/pages/chat/page.tsx
@@ -1,31 +1,55 @@
-import { Fragment } from 'react'
+import { ChangeEvent, Fragment, useState } from 'react'
+import axios from 'axios'
 import styles from './chat.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-export default  function Chat () {
-  
+export default function Chat () {
+  const [isEnable, setIsEnable] = useState(true)
+  const [chat, setChat] = useState<string[]>([])  // ChatGPT: #, User: $
+  let _chat: string[]
+  const [question, setQuestion] = useState('')
+  const [lang, setLang] = useState('en')
+
+  const getAnswer = async () => {
+    setIsEnable(false)
+    setChat([...chat, question])
+    _chat = chat
+    _chat.push(question)
+
+    axios.post('http://54.180.126.168:5003/gpt', { language: 'en', prompt: question }, {
+      headers: { 'Content-Type': 'application/json' }
+    }).then(resp => {
+      setChat([..._chat, `#${resp.data.answer}`])
+      setIsEnable(true)
+    }).catch(() => console.error)
+  }
+
   return (
     <Fragment>
       <div className={styles.container}>
         <div className={styles.left}>
-          <div className={styles.chat}>Hello, World!!</div>
+          <div className={styles.chat}>{ lang == 'ko' ? '안녕하세요! 무엇을 도와드릴까요?' : 'Hello, How can i help you?' }</div>
         </div>
-        <div className={styles.left}>
-          <div className={styles.chat}>Pets Care Project with ChatGPT API & Kakao Map API</div>
-        </div>
+        
+        { chat.map((el: string, idx) => 
+          <div className={el[0] == '#' ? styles.left : styles.right} key={idx}>
+            <div className={styles.chat}>{ el.split('#') }</div>
+          </div>
+        ) }
 
-        <div className={styles.right}>
-          <div className={styles.me}>안녕하세요.</div>
-        </div>
-        <div className={styles.right}>
-          <div className={styles.me}>ChatGPT API와 카카오 맵 API를 통한 펫 케어 서비스</div>
-        </div>
+        { !isEnable ? <div className={styles.left}>
+          <div className={styles.chat}>답변중...</div>
+        </div> : null }
       </div>
 
+      <div className={styles.lang}>
+        <button className={lang == 'en' ? styles.selen : styles.en} onClick={() => setLang('en')}>EN</button>
+        <button className={lang == 'ko' ? styles.selko : styles.ko} onClick={() => setLang('ko')}>KO</button>
+      </div>
       <div className={styles.send}>
-        <input className={styles.input} placeholder='질문해보세요.' />
-        <button className={styles.sender}><FontAwesomeIcon icon={faArrowRight} className={styles.arrow} /></button>
+        { isEnable ? <input className={styles.input} placeholder='질문해보세요.' value={question} onChange={(e: ChangeEvent<HTMLInputElement>) => {setQuestion(e.target.value)}} /> : <input className={styles.inputd} value='답변중...' disabled /> }
+        { isEnable ? <button className={styles.sender}><FontAwesomeIcon icon={faArrowRight} className={styles.arrow} onClick={getAnswer} /></button> : <button className={styles.senderd}><FontAwesomeIcon icon={faArrowRight} className={styles.arrow} /></button> }
       </div>
     </Fragment>
   )
