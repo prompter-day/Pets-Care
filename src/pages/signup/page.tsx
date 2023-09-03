4fd7a3da4088bf97f0b83f958def56d311fc4d3f
손보석 <dya_only@dyaonlys-MacBook-Pro.local>
2023-09-03 17:42:13 +0900

    FIX: Remove unused

--- src/pages/signup/page.tsx
+++ src/pages/signup/page.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Fragment, useEffect, useState } from 'react'
+import { ChangeEvent, Fragment, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import styles from './signup.module.css'
